2679608ad11806175ede4f1897133f4139a40f47
rudra <rudrasatasiya@arccus.in>
2025-06-21 16:40:57 +0530

    this is my first commit

---
+++ lib/core/generated/intl/messages_en.dart
@@ -0,0 +1,69 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "lbl_add_product": MessageLookupByLibrary.simpleMessage("Add Product"),
+        "lbl_address": MessageLookupByLibrary.simpleMessage("Address"),
+        "lbl_apartment": MessageLookupByLibrary.simpleMessage("Apartment"),
+        "lbl_app_name": MessageLookupByLibrary.simpleMessage("Dream Vila"),
+        "lbl_bedroom": MessageLookupByLibrary.simpleMessage("Bad Room"),
+        "lbl_confirm_password":
+            MessageLookupByLibrary.simpleMessage("Confirm Password"),
+        "lbl_description": MessageLookupByLibrary.simpleMessage("Description"),
+        "lbl_discount": MessageLookupByLibrary.simpleMessage("Discount"),
+        "lbl_discount_percentage":
+            MessageLookupByLibrary.simpleMessage("Discount Percentage"),
+        "lbl_drawing": MessageLookupByLibrary.simpleMessage("Drawing"),
+        "lbl_email": MessageLookupByLibrary.simpleMessage("Email"),
+        "lbl_first_name": MessageLookupByLibrary.simpleMessage("First Name"),
+        "lbl_gender": MessageLookupByLibrary.simpleMessage("Gender"),
+        "lbl_hall": MessageLookupByLibrary.simpleMessage("Hall"),
+        "lbl_hello": MessageLookupByLibrary.simpleMessage("Hello!"),
+        "lbl_hobby": MessageLookupByLibrary.simpleMessage("Hobby"),
+        "lbl_house": MessageLookupByLibrary.simpleMessage("House"),
+        "lbl_images": MessageLookupByLibrary.simpleMessage("Images"),
+        "lbl_kitchen": MessageLookupByLibrary.simpleMessage("Kitchen"),
+        "lbl_land": MessageLookupByLibrary.simpleMessage("Land"),
+        "lbl_last_name": MessageLookupByLibrary.simpleMessage("Last Name"),
+        "lbl_mobil": MessageLookupByLibrary.simpleMessage("Mobil"),
+        "lbl_office": MessageLookupByLibrary.simpleMessage("Office"),
+        "lbl_password": MessageLookupByLibrary.simpleMessage("Password"),
+        "lbl_photography": MessageLookupByLibrary.simpleMessage("Photography"),
+        "lbl_plot": MessageLookupByLibrary.simpleMessage("Plot"),
+        "lbl_plots": MessageLookupByLibrary.simpleMessage("Plots"),
+        "lbl_price": MessageLookupByLibrary.simpleMessage("Price"),
+        "lbl_rating": MessageLookupByLibrary.simpleMessage("Rating"),
+        "lbl_reading": MessageLookupByLibrary.simpleMessage("Reading"),
+        "lbl_select_image":
+            MessageLookupByLibrary.simpleMessage("Select Image"),
+        "lbl_sign_in": MessageLookupByLibrary.simpleMessage("Sign In"),
+        "lbl_sign_up": MessageLookupByLibrary.simpleMessage("Sign Up"),
+        "lbl_thumbnail": MessageLookupByLibrary.simpleMessage("Thumbnail"),
+        "lbl_title": MessageLookupByLibrary.simpleMessage("Title"),
+        "lbl_type": MessageLookupByLibrary.simpleMessage("Type"),
+        "lbl_upload_user_profile":
+            MessageLookupByLibrary.simpleMessage("Upload User Profile"),
+        "lbl_washroom": MessageLookupByLibrary.simpleMessage("Washroom"),
+        "lbl_writing": MessageLookupByLibrary.simpleMessage("Writing")
+      };
+}
---
+++ lib/core/generated/l10n.dart
@@ -0,0 +1,468 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class Lang {
+  Lang();
+
+  static Lang? _current;
+
+  static Lang get current {
+    assert(_current != null,
+        'No instance of Lang was loaded. Try to initialize the Lang delegate before accessing Lang.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<Lang> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = Lang();
+      Lang._current = instance;
+
+      return instance;
+    });
+  }
+
+  static Lang of(BuildContext context) {
+    final instance = Lang.maybeOf(context);
+    assert(instance != null,
+        'No instance of Lang present in the widget tree. Did you add Lang.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static Lang? maybeOf(BuildContext context) {
+    return Localizations.of<Lang>(context, Lang);
+  }
+
+  /// `Dream Vila`
+  String get lbl_app_name {
+    return Intl.message(
+      'Dream Vila',
+      name: 'lbl_app_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign In`
+  String get lbl_sign_in {
+    return Intl.message(
+      'Sign In',
+      name: 'lbl_sign_in',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign Up`
+  String get lbl_sign_up {
+    return Intl.message(
+      'Sign Up',
+      name: 'lbl_sign_up',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `First Name`
+  String get lbl_first_name {
+    return Intl.message(
+      'First Name',
+      name: 'lbl_first_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Last Name`
+  String get lbl_last_name {
+    return Intl.message(
+      'Last Name',
+      name: 'lbl_last_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Email`
+  String get lbl_email {
+    return Intl.message(
+      'Email',
+      name: 'lbl_email',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Mobil`
+  String get lbl_mobil {
+    return Intl.message(
+      'Mobil',
+      name: 'lbl_mobil',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Gender`
+  String get lbl_gender {
+    return Intl.message(
+      'Gender',
+      name: 'lbl_gender',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Upload User Profile`
+  String get lbl_upload_user_profile {
+    return Intl.message(
+      'Upload User Profile',
+      name: 'lbl_upload_user_profile',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Select Image`
+  String get lbl_select_image {
+    return Intl.message(
+      'Select Image',
+      name: 'lbl_select_image',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password`
+  String get lbl_password {
+    return Intl.message(
+      'Password',
+      name: 'lbl_password',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Confirm Password`
+  String get lbl_confirm_password {
+    return Intl.message(
+      'Confirm Password',
+      name: 'lbl_confirm_password',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hobby`
+  String get lbl_hobby {
+    return Intl.message(
+      'Hobby',
+      name: 'lbl_hobby',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Reading`
+  String get lbl_reading {
+    return Intl.message(
+      'Reading',
+      name: 'lbl_reading',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Drawing`
+  String get lbl_drawing {
+    return Intl.message(
+      'Drawing',
+      name: 'lbl_drawing',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Writing`
+  String get lbl_writing {
+    return Intl.message(
+      'Writing',
+      name: 'lbl_writing',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Photography`
+  String get lbl_photography {
+    return Intl.message(
+      'Photography',
+      name: 'lbl_photography',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hello!`
+  String get lbl_hello {
+    return Intl.message(
+      'Hello!',
+      name: 'lbl_hello',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `House`
+  String get lbl_house {
+    return Intl.message(
+      'House',
+      name: 'lbl_house',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Apartment`
+  String get lbl_apartment {
+    return Intl.message(
+      'Apartment',
+      name: 'lbl_apartment',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Office`
+  String get lbl_office {
+    return Intl.message(
+      'Office',
+      name: 'lbl_office',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Land`
+  String get lbl_land {
+    return Intl.message(
+      'Land',
+      name: 'lbl_land',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Plots`
+  String get lbl_plots {
+    return Intl.message(
+      'Plots',
+      name: 'lbl_plots',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Discount`
+  String get lbl_discount {
+    return Intl.message(
+      'Discount',
+      name: 'lbl_discount',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Title`
+  String get lbl_title {
+    return Intl.message(
+      'Title',
+      name: 'lbl_title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Description`
+  String get lbl_description {
+    return Intl.message(
+      'Description',
+      name: 'lbl_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Address`
+  String get lbl_address {
+    return Intl.message(
+      'Address',
+      name: 'lbl_address',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Price`
+  String get lbl_price {
+    return Intl.message(
+      'Price',
+      name: 'lbl_price',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Discount Percentage`
+  String get lbl_discount_percentage {
+    return Intl.message(
+      'Discount Percentage',
+      name: 'lbl_discount_percentage',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Rating`
+  String get lbl_rating {
+    return Intl.message(
+      'Rating',
+      name: 'lbl_rating',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Plot`
+  String get lbl_plot {
+    return Intl.message(
+      'Plot',
+      name: 'lbl_plot',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Type`
+  String get lbl_type {
+    return Intl.message(
+      'Type',
+      name: 'lbl_type',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Bad Room`
+  String get lbl_bedroom {
+    return Intl.message(
+      'Bad Room',
+      name: 'lbl_bedroom',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hall`
+  String get lbl_hall {
+    return Intl.message(
+      'Hall',
+      name: 'lbl_hall',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Kitchen`
+  String get lbl_kitchen {
+    return Intl.message(
+      'Kitchen',
+      name: 'lbl_kitchen',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Washroom`
+  String get lbl_washroom {
+    return Intl.message(
+      'Washroom',
+      name: 'lbl_washroom',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Thumbnail`
+  String get lbl_thumbnail {
+    return Intl.message(
+      'Thumbnail',
+      name: 'lbl_thumbnail',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Images`
+  String get lbl_images {
+    return Intl.message(
+      'Images',
+      name: 'lbl_images',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Add Product`
+  String get lbl_add_product {
+    return Intl.message(
+      'Add Product',
+      name: 'lbl_add_product',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<Lang> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<Lang> load(Locale locale) => Lang.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
